3c38d0aa7c3330d2c769a9e8805c149ead7667ae
Prannay Khosla <prannay.khosla@gmail.com>
2017-01-28 20:43:46 +0530

    Added init for crnn

---
+++ init.lua
@@ -0,0 +1,13 @@
+require ('torch')
+require ('nn')
+require ('nngraph')
+require ('lfs')
+require ('optim')
+
+crnn = {}
+
+require('crnn.util')
+require('crnn.model')
+require('crnn.data')
+
+return crnn
