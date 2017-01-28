03bab7449e49daa79ec217670cea9893952c202a
Prannay Khosla <prannay.khosla@gmail.com>
2017-01-28 21:14:02 +0530

    Working model of library

---
+++ data/init.lua
@@ -0,0 +1,22 @@
+package = "LuaFruits"
+version = "1.0-1"
+source = {
+    url = "https://github.com/prannayk/char-rnn.git"
+}
+description = {
+    summary = "Character level Recurrent Neural Networks",
+    detailed = [[
+        This is is based on nn and nn graphs and trains models for sequence to sequence learning. 
+    ]],
+    homepage = "https://github.com/karpathy/char-rnn",
+    license = "MIT/X11"
+}
+dependencies = {
+    "lua >= 5.1, <5.4"
+}
+build = {
+    type = "builtin",
+    modules = {
+        crnn = "./init.lua"
+    }
+}
--- util/init.lua
+++ util/init.lua
@@ -5,7 +5,7 @@ require 'optim'
 
 crnn.util = {}
 
-require('crnn.util.CharSplitMMinibatchLoader')
+require('crnn.util.CharSplitLMMinibatchLoader')
 require('crnn.util.OneHot')
 require('crnn.util.misc')
 require('crnn.util.model_utils')
